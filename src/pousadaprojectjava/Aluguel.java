960442a9dfffba7c0c233021ae5900d5b205e298
DStalkerBR <34558728+DStalkerBR@users.noreply.github.com>
2017-12-15 02:06:39 -0200

    Update Aluguel.java

--- src/pousadaprojectjava/Aluguel.java
+++ src/pousadaprojectjava/Aluguel.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pousadaprojectjava;
-import java.util.Date;
+import java.time.LocalDate;
 
 /**
  *
@@ -17,9 +17,54 @@ import java.util.Date;
  */
 
 public class Aluguel {
-    Cliente cliente;
-    Quarto aluguelQ;
-    Date checkIn;
-    Date checkOut;
-    
+    private int idAluguel;
+    private Cliente cliente;
+    private Quarto quarto;
+    private int dias;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    public Aluguel(int idAluguel, Cliente cliente, Quarto quarto, LocalDate checkIn, LocalDate checkOut) {
+        this.idAluguel = idAluguel;
+        this.cliente = cliente;
+        this.quarto = quarto;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public boolean estaOcupadoNaData(CollectionAlugueis colAlugueis) {
+        for (Aluguel existente: colAlugueis.getListaAlugueis()) {
+            if (!(this.checkIn.isAfter(existente.getCheckOut()) || this.checkOut.isBefore(existente.getCheckIn())))
+                return true;
+        }
+        return false;
+    }
+    public int diasTotal() {
+        //this.dias
+        return dias;
+    }
+
+    public int getIdAluguel() {
+        return idAluguel;
+    }
+
+    public void setIdAluguel(int idAluguel) {
+        this.idAluguel = idAluguel;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalDate checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalDate checkOut) {
+        this.checkOut = checkOut;
+    }
 }
